8c74151bcaaa4e1039b0b598b9312cdabffa76f1
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-03-21 02:58:52 -0300

    funciona info y storage, pero no imagen guardada en campo url

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,117 @@
-const miFuncionTesteable = () => {
-  console.log("Holo, soy una función");
-  return true;
+
+let newBusinessKey = firebase.database().ref('users/boasfdisfbsfahb').child('products').push().key;
+console.log(newBusinessKey);
+
+firebase.database().ref('users/' + newBusinessKey).set({
+  campo: newBusinessKey
+})
+
+
+
+/* let newBusinessKey = ""; */
+const saveBusiness = (/* businessImage, */ businessName, businessDescription, businessCategory, businessPhone, businessCity) => {
+  /* let */ newBusinessKey = firebase.database().ref('business/boasfdisfbsfahb').child('products').push().key;
+  console.log(newBusinessKey);
+  firebase.database().ref('business/' + newBusinessKey).set({
+    /* image: businessImage, */
+    owner: businessName,
+    description: businessDescription,
+    key: newBusinessKey,
+    category: businessCategory,
+    phone: businessPhone,
+    city: businessCity
+  });
 };
 
-const miOtraFunctionTesteable = () => {
-  return false;
-};
+/* function onLikeClick(e) {
+  let key = e.target.id;
+  let oriKey = e.target.oid;
+  let numberKey = e.target.nid;
+  document.getElementById(key).style.background = "lightblue";
+  firebase.database().ref(`recipe/${oriKey}`).child(`likes/${firebase.auth().currentUser.uid}`).set({
+    user: firebase.auth().currentUser.uid,
+  });
+  firebase.database().ref(`recipe/${oriKey}`).child("likes").on("value", function (snapshot) {
+    firebase.database().ref(`recipe/${oriKey}`).update({
+      likesCount: snapshot.numChildren(),
+    })
+    console.log("There are " + snapshot.numChildren() + " likes");
+    document.getElementById(numberKey).innerHTML = "";
+    document.getElementById(numberKey).innerHTML = snapshot.numChildren();
+  });
+} */
 
 
-// Importante agregar la función que se conectará con el dom desde javascript
-// O no funcionará
-if( document.getElementById('miBoton') ) {
-  document.getElementById('miBoton').addEventListener('click', miFuncionTesteable);
+var uploader = document.getElementById('uploader');
+var fileButton = document.getElementById('fileButton');
+let imgUrl = "";
+
+fileButton.addEventListener('change', function (e) {
+  var file = e.target.files[0];
+  var storageRef = firebase.storage().ref('Img/'
+    + file.name);
+  var task = storageRef.put(file);
+  task.on('state_changed',
+    function progress(snapshot) {
+      var percentage = (snapshot.bytesTransferred /
+        snapshot.totalBytes) * 100;
+      uploader.value = percentage;
+    },
+    function error(err) {
+    },
+    function complete() {
+    }
+  );
+  storageRef.getDownloadURL().then(function (url) {
+    console.log(newBusinessKey);
+    console.log(url);
+    imgUrl = url;
+    /* firebase.database().ref(`recipe/${newRecipeKey}`).update({
+        urlImage : url 
+      });*/
+    // Insert url into an <img> tag to "download"
+  }).catch(function (error) {
+
+    // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+    switch (error.code) {
+      case 'storage/object-not-found':
+        // File doesn't exist
+        break;
+
+      case 'storage/unauthorized':
+        // User doesn't have permission to access the object
+        break;
+
+      case 'storage/canceled':
+        // User canceled the upload
+        break;
+
+      case 'storage/unknown':
+        // Unknown error occurred, inspect the server response
+        break;
+    }
+  });
+})
+
+
+
+const saveBusinessIntoDatabase = () => {
+  /* const businessImage = imgURL; */
+  const businessName = document.getElementById("name").value;
+  const businessDescription = document.getElementById("description").value;
+  const businessCategory = document.getElementById("category").value;
+  const businessPhone = document.getElementById("phone").value;
+  const businessCity = document.getElementById("city").value;
+  /* const recipeTitle = titleRecipe.value;
+  const recipeImage = imgUrl;
+  const ownerName = firebase.auth().currentUser.email; */
+  saveBusiness(/* businessImage, */ businessName, businessDescription, businessCategory, businessPhone, businessCity);
 }
 
-export const miFuncionTesteableExp = miFuncionTesteable;
-export const miOtraFunctionTesteableExp = miOtraFunctionTesteable;
+document.getElementById("sendBusiness").addEventListener('click', saveBusinessIntoDatabase);
+
+
+
+
+
